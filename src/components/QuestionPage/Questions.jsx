6e7b2c2f1d19fd7ae0f2e6fa636acfbdfa6df011
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-01-08 13:44:50 +0000

    Update Questions.jsx

--- src/components/QuestionPage/Questions.jsx
+++ src/components/QuestionPage/Questions.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { Container, Form, Row, Col, Button } from "react-bootstrap";
+import {
+  Container,
+  Form,
+  Row,
+  Col,
+  Button,
+  ProgressBar,
+} from "react-bootstrap";
 
 class Questions extends React.Component {
   state = {
@@ -29,7 +36,28 @@ class Questions extends React.Component {
 
   submitAnswer = (e) => {
     e.preventDefault();
+    this.sendAnswer();
+    this.setState({ isSelected: false });
     this.props.nextQuestion();
+  };
+
+  sendAnswer = async () => {
+    try {
+      let response = await fetch(
+        `http://localhost:8008/exams/${this.props.examID}/answer`,
+        {
+          method: "POST",
+          body: JSON.stringify(this.state.answer),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      let res = res.body;
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   render() {
@@ -37,6 +65,7 @@ class Questions extends React.Component {
       <Container>
         {console.log(this.props.question)}
         <Row>{this.props.question.text}</Row>
+
         <Row>
           <Form onSubmit={this.submitAnswer}>
             <Form.Group as={Row}>
