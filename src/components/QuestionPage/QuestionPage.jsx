50cccb5aa888594fdd788a533589b27bcd002830
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-01-08 10:01:43 +0000

    START PAGE FETCH ADDED

---
+++ src/components/QuestionPage/QuestionPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class QuestionPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>QUESTIONS GO HERE</h1>
+      </div>
+    );
+  }
+}
+
+export default QuestionPage;
